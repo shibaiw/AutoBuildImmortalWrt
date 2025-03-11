95b07a9d0cff155b9cf651fd1762546e3567d0dc
shibaiw <167851515+shibaiw@users.noreply.github.com>
2025-03-11 12:10:49 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -48,7 +48,7 @@ PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ipsec-vpnserver-manyusers-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-openvpn-server-zh-cn"
+
 
 
 
