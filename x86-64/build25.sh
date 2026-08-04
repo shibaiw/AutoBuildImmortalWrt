73a6189d4199b26223f684385e1d1fe4e6dbf8b2
shibaiw <167851515+shibaiw@users.noreply.github.com>
2026-08-04 11:30:28 +0800

    Add additional language packages for luci

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -57,6 +57,12 @@ PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-snmpd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
+
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # ======== shell/apk-custom-packages.sh =======
