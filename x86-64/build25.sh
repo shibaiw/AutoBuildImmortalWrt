9b3d026f9c55b1bbba57ec77f8a4547df41014b2
shibaiw <167851515+shibaiw@users.noreply.github.com>
2026-09-21 18:52:25 +0800

    Update build25.sh

--- x86-64/build25.sh
+++ x86-64/build25.sh
@@ -59,7 +59,7 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-snmpd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
